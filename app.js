60d47bbc3a4b559e67bcb84a5085668caf4baa64
myashjain10 <yashjain2312@outlook.com>
2024-11-25 16:12:00 +0530

    Added routes and mongodb connection

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
-
 const express = require("express");
 const app = express();
+const db = require("./config/mongoose-connection")
+const ownersRouter = require("./routes/ownersRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +16,6 @@ app.set("view engine", "ejs");
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
-
-
-
 
 
 app.listen(3000, (err)=>{
